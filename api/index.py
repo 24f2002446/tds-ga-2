26e0e49aa8765305ac9c43352e8404519e8ef231
24f2002446 <24f2002446@ds.study.iitm.ac.in>
2025-10-05 11:00:16 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,45 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import json
+import math
 
 app = FastAPI()
 
+class Data(BaseModel):
+    regions: list[str]
+    threshold_ms: int
+
+def get_p95_lat(all_lats):
+    index = (len(all_lats) - 1) * 0.95
+    if index.is_integer():
+        return all_lats[int(index)]
+    else:
+        lower_value = all_lats[int(math.floor(index))]
+        upper_value = all_lats[int(math.ceil(index))]
+        return lower_value + (upper_value - lower_value) * (index - int(math.floor(index)))
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/post")
+async def post(data: Data):
+    item = data.model_dump()
+
+    with open("q-vercel-latency.json", "r") as f:
+        regions_data = json.load(f).get("regions", [])
+
+    result = []
+    for region in item.regions:
+        matches = [obj for obj in regions_data if obj.get("region") == region]
+        matches.sort(key=lambda x: x.latency_ms)
+
+        avg_latency = sum([obj.get("latency_ms") for obj in matches]) / len(matches)
+        p95_latency = get_p95_lat(list(map(lambda x: x.get("latency_ms"), list(matches))))
+        avg_uptime = sum([obj.get("uptime_pct") for obj in matches]) / len(matches)
+        breaches = len([obj for obj in matches if obj.get("latency_ms") > item.threshold_ms])
+
+        res = {"region": region, "avg_latency": avg_latency, "p95_latency": p95_latency, "avg_uptime": avg_uptime, "breaches": breaches}
+        result.append(res)
+
+    return result
