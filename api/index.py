6c312d2ac21358d6a7ca16142f2e02fda7f306ba
24f2002446 <24f2002446@ds.study.iitm.ac.in>
2025-10-05 11:02:03 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import json
 import math
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"], # Allows all methods
+    allow_headers=["*"], # Allows all headers
+)
 
 class Data(BaseModel):
     regions: list[str]
